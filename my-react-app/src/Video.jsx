bc8fbcc51e47c377c59ab8a20319718f4a01c422
Alberto Ruben Echavarria Zalazar <94318456+alberto012@users.noreply.github.com>
2024-05-12 11:54:55 -0300

    arreglos y actualizaciones

--- my-react-app/src/Video.jsx
+++ my-react-app/src/Video.jsx
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import Modal from './Modal';
-import ProgressBar from './Progreso'; // Importamos el componente ProgressBar
-import futuro from '../images/futuro.jfif';
+import VideoSpinner from './tools/Spinner';
 
 const VideoPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [answered, setAnswered] = useState({
-    question1: false,
-    question2: false,
-    question3: false,
-    question4: false
-  });
   const [videoPaused, setVideoPaused] = useState(false);
   const [videoStarted, setVideoStarted] = useState(false);
   const [lastModalTime, setLastModalTime] = useState(0);
-
-  useEffect(() => {
-    if (showModal) {
-      setVideoPaused(true);
-    }
-  }, [showModal]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleProgress = ({ playedSeconds }) => {
     const currentSecond = Math.floor(playedSeconds);
@@ -31,42 +19,57 @@ const VideoPage = () => {
     }
   };
 
-  const handleCloseModal = (isTrue) => {
+  const handleCloseModal = () => {
     setShowModal(false);
-    const question = isTrue ? 'question1' : 'question2';
-    setAnswered(prevState => ({
-      ...prevState,
-      [question]: true
-    }));
     setVideoPaused(false);
   };
 
   const handleSubmitModal = (question, answer) => {
-    // Aquí puedes manejar la lógica para enviar la respuesta a tu backend, por ejemplo:
     console.log(`Question ${question} answered with ${answer}`);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <div className="text-center">
-        <h1 className="text-4xl text-white mb-8 font-bold">This is the Video Page!</h1>
-        <div className="mb-8">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            controls
-            onProgress={handleProgress}
-            onPlay={() => setVideoStarted(true)}
-            playing={!videoPaused}
-            width="1040px"
-            height="640px"
-            style={{ border: '1px solid #4a4a4a', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', filter: videoStarted ? 'none' : 'brightness(0.5)' }}
-          />
-          {/* <ProgressBar totalQuestions={4} answered={answered} /> */}
+    <>
+      {isLoading ? (
+        <VideoSpinner />
+      ) : (
+        <div className="flex justify-center items-center h-screen bg-gray-900">
+          <div className="text-center">
+            <h1 className="text-4xl text-white mb-8 font-bold">This is the Video Page!</h1>
+            <div className="mb-8">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                playing={!videoPaused}
+                width="100%"
+                height="100%"
+                style={{
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                }}
+                onProgress={handleProgress}
+                onPlay={() => setVideoStarted(true)}
+                config={{
+                  youtube: {
+                    playerVars: {
+                      modestbranding: 1,
+                    },
+                  },
+                }}
+              />
+            </div>
+            <p className="text-lg text-gray-400">Here you can add your video content.</p>
+            <Modal show={showModal} onClose={handleCloseModal} onSubmit={handleSubmitModal} />
+          </div>
         </div>
-        <p className="text-lg text-gray-400">Here you can add your video content.</p>
-        <Modal show={showModal} onClose={handleCloseModal} onSubmit={handleSubmitModal} />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
